68228f6e94fae55f5b692729f4dc5b0023fdc0ee
郁世昌 <yushichang0845@dingtalk.com>
2019-04-28 18:12:24 +0800

    测试

--- client/app.vue
+++ client/app.vue
@@ -12,7 +12,7 @@ import Header from './layout/header.vue';
 import Footer from './layout/footer.jsx';
 import Todo from './views/todo/todo.vue';
 
-console.log(sadassfsffa);
+console.log(sadaasssfsffa);
 
 export default {
 	components: {
