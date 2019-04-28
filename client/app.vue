39690c9eac5adf76627e360e401760c0a73ae8a1
郁世昌 <yushichang0845@dingtalk.com>
2019-04-28 17:37:13 +0800

    测试

--- client/app.vue
+++ client/app.vue
@@ -12,7 +12,7 @@ import Header from './layout/header.vue';
 import Footer from './layout/footer.jsx';
 import Todo from './views/todo/todo.vue';
 
-console.log(aaaa);
+console.log(aa);
 
 export default {
 	components: {
