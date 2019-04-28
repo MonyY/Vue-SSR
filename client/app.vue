99e4ca6548726a65857ccfe98219b1cb34672d2f
郁世昌 <yushichang0845@dingtalk.com>
2019-04-28 17:36:51 +0800

    测试husky

--- client/app.vue
+++ client/app.vue
@@ -12,7 +12,7 @@ import Header from './layout/header.vue';
 import Footer from './layout/footer.jsx';
 import Todo from './views/todo/todo.vue';
 
-console.log(aaa);
+console.log(aaaa);
 
 export default {
 	components: {
